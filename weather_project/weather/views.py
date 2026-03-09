fc886746cf351ce009aee20060b6f0f6e948eb95
Akshaya122019 <geoakshay2002@gmail.com>
2026-03-09 19:25:20 +0530

    weather

--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -5,32 +5,39 @@ from django.conf import settings
 
 def home(request):
 
-    city = request.GET.get("city", "Chennai")
+    city = request.GET.get("city")
+    lat = request.GET.get("lat")
+    lon = request.GET.get("lon")
 
-    # Current weather API
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={settings.API_KEY}&units=metric"
+    if lat and lon:
+
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={settings.API_KEY}&units=metric"
+
+    else:
+
+        city = city or "Chennai"
+
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={settings.API_KEY}&units=metric"
+
+
     response = requests.get(url)
     data = response.json()
 
-    # Forecast API
-    forecast_url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={settings.API_KEY}&units=metric"
-    forecast_response = requests.get(forecast_url)
-    forecast_data = forecast_response.json()
 
-    if data.get("cod") != 200:
-        return render(request, "home.html", {
-            "error": data.get("message")
-        })
+    forecast_url = f"https://api.openweathermap.org/data/2.5/forecast?q={data['name']}&appid={settings.API_KEY}&units=metric"
 
-    weather_main = data["weather"][0]["main"]
+    forecast = requests.get(forecast_url).json()
+
 
     weather_data = {
-        "city": city,
+
+        "city": data["name"],
         "temperature": data["main"]["temp"],
         "description": data["weather"][0]["description"],
         "icon": data["weather"][0]["icon"],
-        "weather_main": weather_main,
-        "forecast": forecast_data["list"][:5]
+        "weather_main": data["weather"][0]["main"],
+        "forecast": forecast["list"][:5]
+
     }
 
     return render(request, "home.html", weather_data)
